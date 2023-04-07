8947939a0e4edd4a745034c0d3007f1924dce461
PabMatMar <pabmatmar@gmail.com>
2023-04-07 23:49:40 +0200

    add/ 8kyu Are you Playing Banjo?

---
+++ 8kyu/areYouPlayingBanjo.js
@@ -0,0 +1,19 @@
+https://www.codewars.com/kata/53af2b8861023f1d88000832
+
+// Crea una función que responda a la pregunta "¿Estás tocando el banjo?".
+// Si tu nombre comienza con la letra "R" o "r" minúscula, ¡estás tocando el banjo!
+
+// La función toma un nombre como único argumento y devuelve una de las siguientes cadenas:
+
+// name + " plays banjo" 
+// name + " does not play banjo"
+// Los nombres dados son siempre cadenas válidas.
+
+function areYouPlayingBanjo(name) {
+
+    if (/^[R]/.test(name) || /^[r]/.test(name)) {
+        return name + " plays banjo"
+    } else {
+        return name + " does not play banjo"
+    }
+}
