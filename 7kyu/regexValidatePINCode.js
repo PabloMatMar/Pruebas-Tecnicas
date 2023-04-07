974dc56f4edf4e01d63d49b1a1f1e273f82199e6
PabMatMar <pabmatmar@gmail.com>
2023-04-08 00:13:56 +0200

    add/ 7kyu Regex Validate PIN code

---
+++ 7kyu/regexValidatePINCode.js
@@ -0,0 +1,16 @@
+https://www.codewars.com/kata/55f8a9c06c018a0d6e000132
+
+// Los cajeros automáticos permiten códigos PIN de 4 o 6 dígitos y los códigos PIN no pueden contener nada más que exactamente 4 dígitos o exactamente 6 dígitos.
+
+// Si a la función se le pasa una cadena de PIN válida, devuelva true, de lo contrario, devuelva false.
+
+// Ejemplos ( Entrada --> Salida)
+// "1234"   -->  true
+// "12345"  -->  false
+// "a234"   -->  false
+
+function validatePIN(pin) {
+
+    return /^[0-9]{4,4}$/.test(pin) || /^[0-9]{6,6}$/.test(pin) ? true : false
+
+}
--- 8kyu/reversedStrings.js
+++ 8kyu/reversedStrings.js
@@ -1,3 +1,5 @@
+https://www.codewars.com/kata/5168bb5dfe9a00b126000018
+
 // Complete la solución para que invierta la cadena que se le pasó.
 
 // 'world'  =>  'dlrow'
