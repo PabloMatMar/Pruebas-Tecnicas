b0a8cfe5921cfabcf3ebd8c2a1e732534c997f66
PabMatMar <pabmatmar@gmail.com>
2023-04-08 22:12:16 +0200

    add/ 7kyu Mumbling

---
+++ 7kyu/mumbling.js
@@ -0,0 +1,26 @@
+https://www.codewars.com/kata/5667e8f4e3f572a8f2000039
+
+// Esta vez sin historia, sin teoría. Los siguientes ejemplos le muestran cómo escribir la función accum:
+
+// Ejemplos:
+// accum("abcd") -> "A-Bb-Ccc-Dddd"
+// accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+// accum("cwAt") -> "C-Ww-Aaa-Tttt"
+// El parámetro de accum es una cadena que incluye solo letras de a..zy A..Z.
+
+function accum(s) {
+    let minus = s.toLowerCase();
+    let accum = minus[0].toUpperCase();
+    let solution = [];
+    let arrLetters = minus.split('');
+
+    for (let i = 0; i < arrLetters.length; i++) {
+        for (let j = 0; j < i; j++) {
+            const initialValue = arrLetters[i].toUpperCase();
+            j === 0 ? accum = "-" + initialValue + arrLetters[i] : accum = accum + arrLetters[i];
+        };
+        solution.push(accum);
+    }
+
+    return solution.join('');
+};
