6ce529a1dfc18d5d52cea15117b74f79cba6bdee
PabMatMar <pabmatmar@gmail.com>
2023-04-21 23:59:21 +0200

    add/ 5kyu not very secure

---
+++ 5kyu/notverySecure.js
@@ -0,0 +1,13 @@
+// En este ejemplo, debe validar si una cadena de entrada del usuario es alfanumérica. La cadena dada no es nil/null/NULL/None, por lo que no tiene que verificar eso.
+
+// La cadena tiene las siguientes condiciones para ser alfanumérica:
+
+// Al menos un carácter ( ""no es válido)
+// Los caracteres permitidos son letras latinas en mayúsculas/minúsculas y dígitos de 0a9
+// Sin espacios en blanco/guion bajo
+
+
+function alphanumeric(string) {
+    return /^[A-Za-z0-9]+$/.test(string);
+
+};
