516591317073fc90661c14007886fbf5051c0354
PabMatMar <pabmatmar@gmail.com>
2023-04-21 23:48:13 +0200

    add/ 5kyu Extract Domain

---
+++ 5kyu/extractDomain.js
@@ -0,0 +1,20 @@
+// Escriba una función que, cuando se le proporcione una URL como una cadena, analice solo el nombre de
+// dominio y lo devuelva como una cadena. Por ejemplo:
+
+
+
+// * url = "http://github.com/carbonfive/raygun" -> domain name = "github"
+// * url = "http://www.zombie-bites.com"         -> domain name = "zombie-bites"
+// * url = "https://www.cnet.com"                -> domain name = cnet"
+
+
+
+function domainName(url) {
+    return url
+        .replace("https://", '')
+        .replace("http://", '')
+        .replace("www.", '')
+        .split('.')[0]
+};
+
+//domainName(/*Pásale una url en un string y devolverá el dominio de la misma en otr string */)
