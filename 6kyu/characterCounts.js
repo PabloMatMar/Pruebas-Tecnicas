3aa045a899da1753466423ce1ad69461e55a44a8
PabMatMar <pabmatmar@gmail.com>
2023-05-26 00:10:19 +0200

    add/ statement to 6kyu Character Counts

--- 6kyu/characterCounts.js
+++ 6kyu/characterCounts.js
@@ -1,13 +1,27 @@
+// https://www.codewars.com/kata/56b409febccd5aafbd000021
+
+
+// El objeto es contar el número de apariciones de un carácter específico (o conjunto de caracteres) en una cadena.
+
+// Instrucciones
+// Completa la función de marcador de posición characterCount.
+
+// debe devolver el número de veces que aparece un solo carácter en la cadena, o
+// si se especifican varios caracteres (ya sea proporcionando una matriz o una cadena de longitud 2 o más), debe devolver una matriz de recuentos de caracteres
+// consulte las pruebas unitarias proporcionadas para obtener una definición más completa
+
 String.prototype.characterCount = function (arg) {
-    if (arg == undefined || arg.length == 0) return undefined;
+    if (arg == undefined || arg.length == 0) return;
     let solution = [];
     let letters = "";
+    let count = {};
+    let check = false;
     typeof (arg) == 'string' ? letters = arg.split('') : letters = arg;
-    let count = {}
-    this.toString().split('').map(e => count[e] == undefined ? count[e] = 1 : count[e]++)
-    solution = letters.map(e => count[e] == undefined ? 0 : count[e])
+    this.toString().split('').map(e => count[e] == undefined ? count[e] = 1 : count[e]++);
+    solution = letters.map(e => count[e] == undefined ? 0 : count[e]);
+    if (solution.length > 1) check = true;
     const min = solution.filter(e => e != 0);
-    if (min.length == 0) return 0
-    else if (min.length == 1) return min[0]
-    return solution
+    if (min.length == 0 && !check) return 0;
+    else if (min.length == 1) return min[0];
+    return solution;
 };
